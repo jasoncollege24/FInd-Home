5b1bb8d70e06172693ad6918b8efbc3c0fb04602
jasoncollege24 <jasoncollege24@gmail.com>
2021-05-30 14:12:07 -0400

    Initial Start

---
+++ game.js
@@ -0,0 +1,9 @@
+console.log("After going to sleep in your bed one night, you wake to find yourself in a strange place, with a lit flashlight attached to your shirt pocket.");
+
+console.log("Quickly sitting up, you look around to see that it is still night. It also seems you're in a forest of some type, as you are surrounded by trees as far as you can see.\n");
+
+console.log("Attached to your wrist is something akin to a watch, but with a number that changes as you move in any direction.\n");
+
+console.log("Reaching in your pocket, you find a note: \"Find the portal. It is your only way home. The distance finder on your wrist can help you.\n\"");
+
+console.log("Available actions:\nLook\nDistance\nNorth\nSouth\nEast\nWest");
